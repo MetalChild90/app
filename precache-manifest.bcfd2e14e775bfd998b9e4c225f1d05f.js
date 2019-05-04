ed0c7392680b22bc87f6aa450479eb1a4814687c
Eichan <metalchild90@gmail.com>
2019-05-04 14:02:32 +0100

    Updates

---
+++ precache-manifest.bcfd2e14e775bfd998b9e4c225f1d05f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f02ee9b550ac0eca50fccf1e17d18350",
+    "url": "/app/index.html"
+  },
+  {
+    "revision": "804b23955c232ca22dae",
+    "url": "/app/static/css/main.0f6f97b9.chunk.css"
+  },
+  {
+    "revision": "5dc23cb15a4085df09d1",
+    "url": "/app/static/js/2.e2342db6.chunk.js"
+  },
+  {
+    "revision": "804b23955c232ca22dae",
+    "url": "/app/static/js/main.04e7f197.chunk.js"
+  },
+  {
+    "revision": "127917d16c2343d0c8fa",
+    "url": "/app/static/js/runtime~main.d465ad9e.js"
+  },
+  {
+    "revision": "70e6670ee746fc59f414fde4fcce93af",
+    "url": "/app/static/media/header1.70e6670e.jpg"
+  },
+  {
+    "revision": "33eca35fceea8311d67358a6629fa9d9",
+    "url": "/app/static/media/header2.33eca35f.jpg"
+  },
+  {
+    "revision": "d9d36b65a132d26aed884ad58e4719eb",
+    "url": "/app/static/media/header3.d9d36b65.jpg"
+  }
+]);
